902dfcee75bc91c866a672ebc205ae788008d796
levu123 <levu22032004@gmail.com>
2023-06-10 15:58:26 +0700

    commit do_an_ban_dc

---
+++ php/php/index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="Add To Cardport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="../style.css">
+
+
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <img src="https://w7.pngwing.com/pngs/231/63/png-transparent-album-computer-icons-music-phonograph-record-design-album-photography-logo.png" alt="" style="max-height:100px;width: 100px;">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Trang Chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../html/about_us.html">Giới thiệu</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="../html/products.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Sản Phẩm
+                    </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a class="dropdown-item" href="#">Album Thiếu Nhi</a>
+                                </li>
+                                <li><a class="dropdown-item" href="#">Album Nhạc Pop</a></li>
+                                <li>
+                                    <a class="dropdown-item" href="#">album khác</a>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#spetific_pro.html">Chi Tiết</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#contact.html">Liên Hệ</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+    </div>
+    <div class="khoi_banner">
+        <!-- <div class="slider">
+            <img src="https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/107/posts/28020/final_image/31-clock-flat850-1.jpg" alt="">
+            <img src="https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/107/posts/28020/final_image/31-clock-flat850-1.jpg" alt="">
+            <img src="https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/107/posts/28020/final_image/31-clock-flat850-1.jpg" alt="">
+
+            <img src="https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/107/posts/28020/final_image/31-clock-flat850-1.jpg" alt="">
+        </div> -->
+        <div class="elm-wrapper">
+            <div class="elm-center">
+                <p>Album in the shop</p>
+                <br>welcome
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="main">
+            <h3>Danh Mục Sản Phẩm</h3>
+            <div class=" row danh_muc">
+                <div class="col-md-3">
+                    <div class="square">
+                        <a href=""></a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="square">
+
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="square">
+
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="square">
+
+                    </div>
+                </div>
+
+            </div>
+            <h3>Sản Phẩm Nổi Bật</h3>
+            <div class=" row danh_muc">
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/5/16/805788/BTS-2.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add To Card </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/5/16/805788/BTS-2.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add To Card </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/5/16/805788/BTS-2.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add To Card </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/5/16/805788/BTS-2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add To Card </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <h3>Sản Phẩm Nổi Bật</h3>
+            <div class=" row danh_muc">
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/5/16/805788/BTS-2.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add To Card </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/5/16/805788/BTS-2.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add To Card </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/5/16/805788/BTS-2.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add To Card </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/5/16/805788/BTS-2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add To Card </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row main">
+                <div class="col-md-3">
+                    <img src="https://banner2.cleanpng.com/20180810/jwj/kisspng-clip-art-clock-computer-icons-vector-graphics-down-augic-time-svg-png-icon-free-download-2146-7-5b6e2cf9b6ce96.9921720015339471297488.jpg" alt="" style="width: 40px;height: 40px;">
+                    <h6>10 minute grocery now</h6>
+                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                </div>
+                <div class="col-md-3">
+                    <img src="https://banner2.cleanpng.com/20180810/jwj/kisspng-clip-art-clock-computer-icons-vector-graphics-down-augic-time-svg-png-icon-free-download-2146-7-5b6e2cf9b6ce96.9921720015339471297488.jpg" alt="" style="width: 40px;height: 40px;">
+                    <h6>10 minute grocery now</h6>
+                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                </div>
+                <div class="col-md-3">
+                    <img src="https://banner2.cleanpng.com/20180810/jwj/kisspng-clip-art-clock-computer-icons-vector-graphics-down-augic-time-svg-png-icon-free-download-2146-7-5b6e2cf9b6ce96.9921720015339471297488.jpg" alt="" style="width: 40px;height: 40px;">
+                    <h6>10 minute grocery now</h6>
+                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                </div>
+                <div class="col-md-3">
+                    <img src="https://banner2.cleanpng.com/20180810/jwj/kisspng-clip-art-clock-computer-icons-vector-graphics-down-augic-time-svg-png-icon-free-download-2146-7-5b6e2cf9b6ce96.9921720015339471297488.jpg" alt="" style="width: 40px;height: 40px;">
+                    <h6>10 minute grocery now</h6>
+                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    </div>
+    </div>
+    <div class="container ">
+        <div class="khoi-footer ">
+            <div class="row">
+                <div class="col-md-4 ">
+                    <img src="https://w7.pngwing.com/pngs/231/63/png-transparent-album-computer-icons-music-phonograph-record-design-album-photography-logo.png" alt="" style="max-height:100px;width: 100px;">
+
+                </div>
+                <div class="col-md-4 ">
+                    <i class="fa-solid fa-phone ">Telephone:0123456789</i>
+                    <br><i class="fa-solid fa-envelope ">Email:aptx4869@gmail.com</i>
+                    <br><i class="fa-solid fa-location-dot ">address:</i>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js " integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin=" anonymous " referrerpolicy="no-referrer "></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.js " integrity="sha512-WNZwVebQjhSxEzwbettGuQgWxbpYdoLf7mH+25A7sfQbbxKeS5SQ9QBf97zOY4nOlwtksgDA/czSTmfj4DUEiQ==" crossorigin=" anonymous " referrerpolicy="no-referrer "></script>
+    <script src="../js/slide.js "></script>
+</body>
+
+</html>
---
+++ php/php/products.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="Add To Cardport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="../style.css">
+
+
+</head>
+
+<body>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js " integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin=" anonymous " referrerpolicy="no-referrer "></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.js " integrity="sha512-WNZwVebQjhSxEzwbettGuQgWxbpYdoLf7mH+25A7sfQbbxKeS5SQ9QBf97zOY4nOlwtksgDA/czSTmfj4DUEiQ==" crossorigin=" anonymous " referrerpolicy="no-referrer "></script>
+    <script src="../js/slide.js "></script>
+</body>
+
+</html>
